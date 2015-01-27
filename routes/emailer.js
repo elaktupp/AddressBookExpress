011ece8a9e8e29373006591fe67b46ab79d641c9
elaktupp <elaktupp@@users.noreply.github.com>
2015-01-27 22:19:22 +0200

    Email sending should now work

--- routes/emailer.js
+++ routes/emailer.js
@@ -12,21 +12,24 @@ var emailMessage = {
     sender:String,
     recipient:String,
     subject:String,
-    message:String
+    message:String,
+    id:String
 };
 
 exports.newEmail = function(req,res) {
   
     // First get the user data and take needed information for the email
-    db.BookUser.findOne({username: req.session.username}, 'email host ssl', function(err,data) {
-        
+    db.BookUser.findOne({username: req.session.username}, 'hostUsername hostPassword email host ssl', function(err,data) {
+    
         emailMessage.hostUsername = data.hostUsername;
         emailMessage.hostPassword = data.hostPassword;
         emailMessage.sender = data.email;
         emailMessage.host = data.host;
         emailMessage.ssl = data.ssl;
+        // Take user id, needed to get back to details afterwards
+        emailMessage.id = req.query.id;
     });
-    
+
 // TODO: Since we have this stuff in the users data then there should also be a USER EDITOR !
     
     db.Contact.findById(req.query.id, function(err,data) {
@@ -37,32 +40,22 @@ exports.newEmail = function(req,res) {
         } else {
             // Get the recipient
             emailMessage.recipient = data.email;
+            // We should have everything, next to the email editor
+            res.render('emailer',emailMessage);
         }
     });
-    
-    // We should have everything, next to the email editor
-    res.render('emailer',emailMessage);
 };
 
 exports.sendEmail = function(req,res) {
   
-    // Note. sender ofter equals hostUsername
-    console.log("*** sendEmail "+req.body.hostUsername);
-    console.log("*** sendEmail "+req.body.hostPassword);
-    console.log("*** sendEmail "+req.body.sender);
-    console.log("*** sendEmail "+req.body.host);
-    console.log("*** sendEmail "+req.body.ssl);
-    console.log("*** sendEmail "+req.body.recipient);
-    console.log("*** sendEmail "+req.body.subject);
-    console.log("*** sendEmail "+req.body.message);
-    
+    // Note. sender often equals hostUsername
     var server = email.server.connect({
        user: req.body.hostUsername, 
        password: req.body.hostPassword, 
        host: req.body.host, 
        ssl: req.body.ssl
     });
-    /*
+    
     server.send({
         text: req.body.message, 
         from: req.body.sender, 
@@ -71,22 +64,10 @@ exports.sendEmail = function(req,res) {
         subject: req.body.subject
     }, function(err, message) {
         
-        emailMessage.sender = req.body.sender;
-        emailMessage.host = req.body.host;
-        emailMessage.ssl = req.body.ssl;
-        emailMessage.recipient = req.body.recipient;
-        emailMessage.subject = req.body.subject;
-        emailMessage.message = req.body.message;
-        emailMessage.hostUsername = req.body.hostUsername;
-        emailMessage.hostPassword = req.body.hostPassword;
+        res.redirect('/contact_details?id='+req.body.id);
         
-// TODO: Error message - now we just go back anyway!
-        
-        if (err) {
-            res.render('emailer',emailMessage);
-        } else {
-            res.render('emailer',emailMessage);
-        }
+// TODO: Success & Error messages - now we just go back to details!
+
     });
-    */
+    
 };
